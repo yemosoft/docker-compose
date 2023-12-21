402543fee96117145a901a5475ffaeeee2098963
yemo <yemosoft1991@gmail.com>
2023-12-21 22:14:37 +0300

    l

--- install-ubuntu/install-ubuntu.sh
+++ install-ubuntu/install-ubuntu.sh
@@ -42,8 +42,7 @@ sudo apt-get install -y \
     nano \
     software-properties-common
 
-# Clone the docker-compose repository
-cd /home && git clone https://github.com/yemosoft/docker-compose.git
-cd /home/docker-compose
 
 
+
+sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io && sudo usermod -aG docker $USER
